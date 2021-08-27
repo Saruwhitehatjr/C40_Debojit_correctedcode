7f6fd9dd5548569a6c6eb018abce36586621dd2f
debojit-biswas <biswasdebojit99@gmail.com>
2021-08-27 19:15:02 +0530

    constructor added

--- js/Game.js
+++ js/Game.js
@@ -20,13 +20,26 @@ class Game{
           gameState:state
       })
     }
-    start()
+  async  start()
 {if(gameState===0)
-    {
+    {    player=new Player();
+       // player.getCount();
+       var playerCountRef=await database.ref('playerCount').once("value")
+    
+       if(playerCountRef.exists()){
+           playerCount=playerCountRef.val()
+           player,getCount();
+       }
         form=new Form();
         form.display()
     }
 
+}
+play()
+{
+    form.formHide();
+    textSize(30);
+    text("GAME START",120,100);
 }
 
 }
--- sketch.js
+++ sketch.js
@@ -1,18 +1,29 @@
 var ball;
 var database,position
 var gameState=0;
-var form,player,game
+var form,player,game,playerCount
 function setup(){
     createCanvas(500,500);
-   
+   database=firebase.database();
+   game=new Game();
+   game.getState();
 
 
 database =firebase.database()
 game=new Game()
 game.start();
-
+game.start
 };
 
 function draw(){
-    background("white");
+    if(playerCount===4){
+            game.update(1);
+        }
+if(gameState===1)
+{
+    game.play();
+}
+
+
+        
 }
